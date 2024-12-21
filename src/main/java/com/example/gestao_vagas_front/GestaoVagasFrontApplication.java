ac390bf52ec4df203451128ace7d4589f9370ce0
joschonarth <joschonarth@gmail.com>
2024-12-21 20:31:09 -0300

    chore: initial project setup

---
+++ src/main/java/com/example/gestao_vagas_front/GestaoVagasFrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestao_vagas_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVagasFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVagasFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestao_vagas_front/GestaoVagasFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestao_vagas_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
