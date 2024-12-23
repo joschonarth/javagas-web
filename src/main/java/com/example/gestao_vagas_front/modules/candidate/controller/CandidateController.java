8138a88dbffcd6d677dd813fb3466e6b10a31f65
joschonarth <joschonarth@gmail.com>
2024-12-23 20:39:22 -0300

    feat: create login page

---
+++ src/main/java/com/example/gestao_vagas_front/modules/candidate/controller/CandidateController.java
@@ -0,0 +1,15 @@
+package com.example.gestao_vagas_front.modules.candidate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/candidate")
+public class CandidateController {
+    
+    @GetMapping("/login")
+    public String login() {
+        return "candidate/login";
+    }
+}
